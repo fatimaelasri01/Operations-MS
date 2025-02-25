914f6006e3451d4fb71161dd9a15bf8b8e81a458
Fatima EL ASRI <fatimaelassri041@gmail.com>
2025-02-25 16:11:18 +0000

    initializ project

---
+++ operationsms/src/main/java/pfe/mandomati/operationsms/OperationsmsApplication.java
@@ -0,0 +1,13 @@
+package pfe.mandomati.operationsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsmsApplication.class, args);
+	}
+
+}
